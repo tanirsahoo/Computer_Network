7243a6062f19ffbfe4faa85387c0eb8946f02612
Tanir Sahoo <121476039+tanirsahoo@users.noreply.github.com>
2024-05-29 01:40:59 +0530

    updates

---
+++ Programs/important_file.py
@@ -0,0 +1,11 @@
+def decimal_to_binary(str):
+    binary = bin(int(str))[2:]
+    return binary
+
+ip = input("Enter the IP Address: ")
+div = ip.split(".")
+print(div)
+for item in div:
+    print(decimal_to_binary(item))
+    print(len(decimal_to_binary(item)))
+    print(int(decimal_to_binary(item), 2))
